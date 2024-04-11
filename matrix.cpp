a494e770659f4b664d31a461233f654313e5d101
Grzegorz Bazior (AGH) <bazior@agh.edu.pl>
2024-04-11 23:55:20 +0200

    Added first implementation for 2024 year

---
+++ matrix.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <stdexcept> // std::out_of_range()
+#include <iomanip>   // std::setw()
+
+using namespace std;
+
+#include "matrix.h"
+
+#warning "Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym"
